506e56945da2713dd29d67cf7cf71c3e713932ad
barry <15914272408@163.com>
2024-04-03 08:10:13 +0800

    test

--- main.py
+++ main.py
@@ -3,23 +3,35 @@ import sys
 import os
 from function import *
 from lib import *
-from button import Button
+from Textbutton import TButton
 
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Adventurer Simulator")
+#font import
+surfont = pygame.font.Font("./VonwaonBitmap-12px.ttf",80)
 #image import
-stbtnN - pygame.image,load("./resources/")
+stbtnN = pygame.transform.scale(pygame.image.load("./resource/background/button/stBtnN.png").convert_alpha(),(200,50))
+stbtnD = pygame.transform.scale(pygame.image.load("./resource/background/button/stBtnD.png").convert_alpha(),(200,50))
+#button
+stbtn = TButton(400,350," ",stbtnN,stbtnD,None,None,surfont,(0,0,0))
+
 
 running = True
 while running:
     for event in pygame.event.get():
+        mx,my = pygame.mouse.get_pos()
         if event.type == pygame.QUIT:
             terminate()
-       # elif event.type == pygame.MOUSEMOTION:
+        elif event.type == pygame.MOUSEMOTION:
+            stbtn.getFocus(mx,my)
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if pygame.mouse.get_pressed()==(1,0,0):
+                stbtn.mouseDown(mx,my)
+        elif event.type == pygame.MOUSEBUTTONUP:
+            stbtn.mouseUp(mx,my)
+        screen.fill((255, 255, 255))
+        stbtn.draw(screen)
 
-
-    screen.fill((255, 255, 255))
     print_text(screen, "AFK Game", 400, 175, 50, (0, 0, 0))
-    print_text(screen, "Dumb ass pophaha137", 400, 350, 20, (0, 0, 0))
     pygame.display.flip()
